1f2ae425d07fc8a3ce8d270f7b213d3e407e7e60
Adharsh T S <adharshsaju.official@gmail.com>
2026-07-11 09:55:56 +0530

    Time: 3 ms (44.25%) | Memory: 8.6 MB (75.77%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.c
@@ -0,0 +1,22 @@
+
+
+char * mergeAlternately(char * word1, char * word2){
+    int i=0,j=0,k=0;
+    int len1 = strlen(word1);
+    int len2 = strlen(word2);
+    int totallen = len1 + len2;
+    char* res = (char*)malloc(sizeof(char) * (totallen + 1));
+    while (i < len1 && j< len2){
+        res[k++] = word1[i++];
+        res[k++] = word2[j++];
+    }
+    while (i < len1){
+        res[k++] = word1[i++];
+    }
+    while (j < len2){
+        res[k++] = word2[j++];
+    }
+    res[k] = '\0';
+    return res;
+
+}
