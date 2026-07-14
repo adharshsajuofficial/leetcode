111b621375d5896c2ade99631a815277078da712
Adharsh T S <adharshsaju.official@gmail.com>
2026-07-14 18:43:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (3.75%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.c
@@ -0,0 +1,18 @@
+bool isSubsequence(char* s, char* t) {
+    int i = strlen(s), j = strlen(t);
+    i--, j--;
+
+    while(i>=0 && j>=0){
+        if(s[i] == t[j]){
+            i--, j--;
+        }
+        else{
+            j--;
+        }
+    }
+    if (i>=0)
+        return false;
+    return true;
+    
+    
+}
